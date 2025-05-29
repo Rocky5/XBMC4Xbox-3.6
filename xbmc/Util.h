2cd0d8c1ae98e126b1b28efdb328dd04d71d9b45
Rocky5 <Rocky5@users.noreply.github.com>
2025-05-29 04:23:49 +0100

    Added support for all retail kernels when using the video switching mode for games. (ported from gamers)

--- xbmc/Util.h
+++ xbmc/Util.h
@@ -92,6 +92,7 @@ public:
   static bool InstallTrainer(CTrainer& trainer);
   static bool RemoveTrainer();
   static bool PatchCountryVideo(F_COUNTRY Country, F_VIDEO Video);
+  static bool CheckForKernelPatchable(int strText);
   static void RunShortcut(const char* szPath);
   static void RunXBE(const char* szPath, char* szParameters = NULL, F_VIDEO ForceVideo=VIDEO_NULL, F_COUNTRY ForceCountry=COUNTRY_NULL, CUSTOM_LAUNCH_DATA* pData=NULL);
   static void LaunchXbe(const char* szPath, const char* szXbe, const char* szParameters, F_VIDEO ForceVideo=VIDEO_NULL, F_COUNTRY ForceCountry=COUNTRY_NULL, CUSTOM_LAUNCH_DATA* pData=NULL); 
